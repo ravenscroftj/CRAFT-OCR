31309626ababd171033d782f10793268c038c761
James Ravenscroft <ravenscroftj@gmail.com>
2024-04-01 18:26:31 +0100

    replace gr.inputs and gr.outputs with new components stuff@

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ def do_ocr(inp):
   bboxes, text = OCR.recoginition(img, results, ocr[0], ocr[1])
   return OCR.visualize(img, results), text
   
-inputs = gr.inputs.Image()
-o1 = gr.outputs.Image()
-o2 = gr.outputs.Textbox()
+inputs = gr.components.Image()
+o1 = gr.components.Image()
+o2 = gr.components.Textbox()
 
 title = "CRAFT TrOCR"
 description = "OCR of both handwriting and printed text using CRAFT Text detector and TrOCR recognition, detection of lines and extraction of them are happening here because TrOCR pre-trained models are modelled on IAM lines dataset and the same needs to be implemented here."
 examples=[['example_1.png'],['example_2.jpg']]
 
 article = "<p style='text-align: center'><a href='https://github.com/Vishnunkumar/craft_hw_ocr' target='_blank'>craft_hw_ocr</a></p><p style='text-align: center'> <p style='text-align: center'><a href='https://github.com/fcakyon/craft-text-detector' target='_blank'>craft-text-detector</a></p><p style='text-align: center'>"
-gr.Interface(fn=do_ocr, inputs=inputs, outputs=[o1, o2], title=title, description=description, article=article, examples=examples, enable_queue=True).launch()
+gr.Interface(fn=do_ocr, inputs=inputs, outputs=[o1, o2], title=title, description=description, article=article, examples=examples).launch()
